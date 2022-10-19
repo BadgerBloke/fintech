1a3376f2ac6be43311f830ece97d51e870ab692c
Mukesh Singh <camukeshkumarsingh@gmail.com>
2022-10-19 22:01:38 +0530

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+/// A fintech library which covers financial along with technical aspect like - you will find functions for financial calculation as-well-as APIs to interact with the Fintech APIs.
+///
+/// # Under Development
+///
+/// This library is under development and will be released with multiple features in future.
+///
+
+fn main() {
+    println!("Hello, world! This is an under development FinTech library.");
+}
